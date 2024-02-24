06dfc5c347d15173b0c551bb571680834c473a81
joNA <jitzian@gmail.com>
2024-02-24 02:13:23 -0500

    rdm jc - Adding GlobalConstans file

---
+++ app/src/main/java/com/org/test/randomusersapp/constants/GlobalConstants.kt
@@ -0,0 +1,10 @@
+package com.org.test.randomusersapp.constants
+
+object GlobalConstants {
+    //Rest
+    const val BASE_URL = "https://randomuser.me/api/"
+
+    //DB
+    const val DB_NAME = "products_db"
+    const val DB_VERSION = 1
+}
