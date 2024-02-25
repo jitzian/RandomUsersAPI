be18398d5a555e66a33b04c5437da5e0c270a2a2
joNA <jitzian@gmail.com>
2024-02-25 10:51:16 -0500

    rdm jc - Adding State for main screen

---
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/main/MainStateUI.kt
@@ -0,0 +1,12 @@
+package com.org.test.randomusersapp.ui.screens.main
+
+import com.org.test.randomusersapp.data.db.entities.ResultEntityDB
+
+sealed class MainStateUI {
+    data object Empty : MainStateUI()
+    data object Loading : MainStateUI()
+    data class Error(val message: String) : MainStateUI()
+    data class Success(
+        val data: List<ResultEntityDB>
+    ) : MainStateUI()
+}
