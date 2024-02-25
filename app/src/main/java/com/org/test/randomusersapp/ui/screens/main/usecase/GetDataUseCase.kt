43065afedc0f3fea44ad5d4c4a22328d10c0b102
joNA <jitzian@gmail.com>
2024-02-25 10:44:12 -0500

    ***rdm jc - Fixing problem of multiple api calls. Now I'm fetching the api first, and in the onSuccess block I'm adding the data so, once everything is already stored in DB, then I'm emiting the data so, it can be shown in the UI

--- app/src/main/java/com/org/test/randomusersapp/ui/screens/main/usecase/GetDataUseCase.kt
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/main/usecase/GetDataUseCase.kt
@@ -4,16 +4,13 @@ import android.util.Log
 import com.org.test.randomusersapp.data.domain.repository.mappers.ResultToResultEntityDBMapper
 import com.org.test.randomusersapp.data.domain.repository.users.UsersRepository
 import com.org.test.randomusersapp.data.remote.model.Result
-import com.org.test.randomusersapp.data.remote.model.ResultApi
 import com.org.test.randomusersapp.ui.screens.main.MainStateUI
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetDataUseCase @Inject constructor(
@@ -32,18 +29,32 @@ class GetDataUseCase @Inject constructor(
         results: Int,
         seed: String,
     ): Flow<MainStateUI> {
-        val data = usersRepository.getUsers(page, results, seed)
-        addResultRoom(data)
-
-        return scope.runCatching { usersRepository.getAllResultsRoom() }
-            .fold(
-                onSuccess = { it.map { item -> MainStateUI.Success(item) } },
-                onFailure = { flowOf(MainStateUI.Error(it.message ?: "Error")) }
-            )
+        return flow {
+            scope.runCatching { fetchRemoteData(page, results, seed) }
+                .fold(
+                    onSuccess = { data ->
+                        addResultRoom(data.results)
+                        emit(
+                            MainStateUI.Success(
+                                data.results.map { resultToResultEntityDBMapper.invoke(it) }
+                            )
+                        )
+                    },
+                    onFailure = { e ->
+                        emit(MainStateUI.Error(e.message ?: "Unknown error"))
+                    }
+                )
+        }
     }
 
     private suspend fun addResultRoom(data: List<Result>) = data.forEach {
         usersRepository.addResultRoom(resultToResultEntityDBMapper.invoke(it))
     }
+
+    private suspend fun fetchRemoteData(
+        page: Int,
+        results: Int,
+        seed: String
+    ) = usersRepository.getUsersData(page, results, seed)
 
 }
