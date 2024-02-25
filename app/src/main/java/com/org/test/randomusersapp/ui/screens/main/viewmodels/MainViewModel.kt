ef952f5db906e7677a42254b28be40693ff11874
joNA <jitzian@gmail.com>
2024-02-24 22:20:12 -0500

    rdm jc - First iteration of MainViewModel that wires the API call and also the storage of the data in the DB

---
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/main/viewmodels/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.org.test.randomusersapp.ui.screens.main.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.org.test.randomusersapp.ui.screens.main.MainStateUI
+import com.org.test.randomusersapp.ui.screens.main.usecase.GetDataUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getDataUseCase: GetDataUseCase,
+) : ViewModel() {
+
+    private val _state: MutableStateFlow<MainStateUI> = MutableStateFlow(MainStateUI.Empty)
+    val state = _state.asStateFlow()
+
+    fun getUsersData(
+        page: Int,
+        results: Int,
+        seed: String,
+    ) = viewModelScope.launch {
+        getDataUseCase.prepareData(page, results, seed).collect {
+            _state.update { it }
+        }
+    }
+}
