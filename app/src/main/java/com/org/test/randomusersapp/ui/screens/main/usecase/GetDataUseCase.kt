2dc153ed125040b744809ab43c54c85468791dfb
joNA <jitzian@gmail.com>
2024-02-24 22:08:14 -0500

    rdm jc - First iteration of use case to retrieve the data and trandform it and save it in the DB

---
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/main/usecase/GetDataUseCase.kt
@@ -0,0 +1,48 @@
+package com.org.test.randomusersapp.ui.screens.main.usecase
+
+import android.util.Log
+import com.org.test.randomusersapp.data.domain.repository.mappers.ResultToResultEntityDBMapper
+import com.org.test.randomusersapp.data.domain.repository.users.UsersRepository
+import com.org.test.randomusersapp.data.remote.model.Result
+import com.org.test.randomusersapp.data.remote.model.ResultApi
+import com.org.test.randomusersapp.ui.screens.main.MainStateUI
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetDataUseCase @Inject constructor(
+    private val usersRepository: UsersRepository,
+    private val resultToResultEntityDBMapper: ResultToResultEntityDBMapper,
+) {
+    private val TAG = GetDataUseCase::class.simpleName
+    private val scope = CoroutineScope(
+        Dispatchers.IO + SupervisorJob() + CoroutineExceptionHandler { _, throwable ->
+            Log.e(TAG, "Handling exception:: ${throwable.message}")
+        }
+    )
+
+    suspend fun prepareData(
+        page: Int,
+        results: Int,
+        seed: String,
+    ): Flow<MainStateUI> {
+        val data = usersRepository.getUsers(page, results, seed)
+        addResultRoom(data)
+
+        return scope.runCatching { usersRepository.getAllResultsRoom() }.fold(
+            onSuccess = { it.map { item -> MainStateUI.Success(item) } },
+            onFailure = { flowOf(MainStateUI.Error(it.message ?: "Error")) }
+        )
+    }
+
+    private suspend fun addResultRoom(data: List<Result>) = data.forEach {
+        usersRepository.addResultRoom(resultToResultEntityDBMapper.invoke(it))
+    }
+
+}
