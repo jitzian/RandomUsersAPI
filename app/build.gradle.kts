9058651f568203492981e8e170c8222e942e7fc6
joNA <jitzian@gmail.com>
2024-02-26 20:08:20 -0500

    rdm jc - Adding toml version catalog. Still some stuff to be cleaned up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,59 +58,58 @@ kapt {
 dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation(libs.androidxCore)
+    implementation(libs.androidxLifecycle)
+    implementation(libs.androidxActivity)
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidxTestExt)
+    androidTestImplementation(libs.androidxTestEspresso)
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-compiler:2.50")
+    implementation(libs.daggerHilt)
+    kapt(libs.hilt.compiler)
 
     // For instrumentation tests
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.50")
-    kaptAndroidTest("com.google.dagger:hilt-compiler:2.50")
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.compiler)
 
     // For local unit tests
-    testImplementation("com.google.dagger:hilt-android-testing:2.50")
-    kaptTest("com.google.dagger:hilt-compiler:2.50")
+    testImplementation(libs.hilt.android.testing)
+    kaptTest(libs.hilt.compiler)
 
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.hiltNavigationCompose)
 
     //okhttp
-    //implementation 'com.squareup.okhttp3:okhttp:4.9.2'
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
+    implementation(libs.okhttp)
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.logging.interceptor)
 
     //Images Compose
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation(libs.coilCompose)
 
     //Room Db
     // Reference: https://dagger.dev/hilt/gradle-setup
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+    implementation(libs.room)
+    kapt(libs.androidx.room.compiler)
 
     // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.room.ktx)
 
     //Kotlin Components and Kotlin Coroutines
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    api(libs.kotlinxCoroutines)
+    api(libs.jetbrains.kotlinx.coroutines.android)
 
     //ConstraintLayout
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation(libs.constraintlayoutCompose)
 }
