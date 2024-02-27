583cd72b6ee7069755b833ee2dac6be615146000
joNA <jitzian@gmail.com>
2024-02-26 19:02:23 -0500

    rdm jc - Cleanup

--- app/src/main/java/com/org/test/randomusersapp/ui/screens/details/usecase/GetDetailDataUseCase.kt
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/details/usecase/GetDetailDataUseCase.kt
@@ -2,39 +2,11 @@ package com.org.test.randomusersapp.ui.screens.details.usecase
 
 import com.org.test.randomusersapp.data.domain.repository.users.UsersRepository
 import com.org.test.randomusersapp.ui.screens.details.state.DetailsStateUI
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetDetailDataUseCase @Inject constructor(
     private val usersRepository: UsersRepository
 ) {
-    suspend fun findResultByIdRoom(id: String): Flow<DetailsStateUI> {
-        return flow {
-            usersRepository.findResultByIdRoom(id).collect {
-                runCatching {
-                    emit(DetailsStateUI.Success(data = it))
-                }.getOrElse { e ->
-                    emit(DetailsStateUI.Error(e.message ?: "Unknown error"))
-                }
-            }
-        }
-    }
-
-    /*suspend fun findResultByValue(value: String): Flow<DetailsStateUI> {
-        return flow {
-            usersRepository.findResultByValue(value).collect {
-                runCatching {
-                    emit(DetailsStateUI.Success(data = it))
-                }.getOrElse { e ->
-                    emit(DetailsStateUI.Error(e.message ?: "Unknown error"))
-                }
-            }
-        }
-    }*/
-
     suspend fun findResultByValue(value: String): DetailsStateUI {
         return runCatching {
             usersRepository.findResultByValue(value)
@@ -43,5 +15,4 @@ class GetDetailDataUseCase @Inject constructor(
             onFailure = { DetailsStateUI.Error(it.message ?: "Unknown error") }
         )
     }
-
 }
