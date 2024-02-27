73cf58b6e0286da79b187bc89a7b944c8eccc28d
joNA <jitzian@gmail.com>
2024-02-27 08:51:49 -0500

    rdm jc - Adding ScrollFloatingButton functionality

--- app/src/main/java/com/org/test/randomusersapp/ui/screens/main/ui/MainScreenBuilder.kt
+++ app/src/main/java/com/org/test/randomusersapp/ui/screens/main/ui/MainScreenBuilder.kt
@@ -1,23 +1,33 @@
 package com.org.test.randomusersapp.ui.screens.main.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.org.test.randomusersapp.data.db.entities.ResultEntityDB
 import com.org.test.randomusersapp.data.db.extensions.idEntityToJson
 import com.org.test.randomusersapp.ui.common.RowItem
+import com.org.test.randomusersapp.ui.common.ScrollFloatingButton
 
 @Composable
 fun MainScreenBuilder(
     modifier: Modifier = Modifier,
     data: List<ResultEntityDB>,
     navigateToDetails: (String) -> Unit,
+    onFetchMore: (() -> Unit)? = null,
 ) {
     val state = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
     Box(modifier = modifier.fillMaxWidth()) {
         LazyColumn(state = state) {
             items(data) { item ->
@@ -25,6 +35,20 @@ fun MainScreenBuilder(
                     data = item
                 ) { navigateToDetails(item.idEntityToJson) }
             }
+            onFetchMore?.let {
+                item {
+                    Text(
+                        text = "More...",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .clickable { onFetchMore() }
+                        ,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
         }
+        ScrollFloatingButton(state = state, coroutineScope = coroutineScope, data = data)
     }
 }
