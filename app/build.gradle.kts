07a1eaffb924c30b8725dccac1072a5ff8c7ab94
joNA <jitzian@gmail.com>
2024-02-26 19:59:43 -0500

    rdm jc - Arranging dependencies just a bit...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,16 @@ android {
         }
     }
 }
+kapt {
+    correctErrorTypes = true
+}
 
 dependencies {
 
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -69,9 +72,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-}
 
-dependencies {
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
 
@@ -84,14 +85,7 @@ dependencies {
     kaptTest("com.google.dagger:hilt-compiler:2.50")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-}
 
-kapt {
-    correctErrorTypes = true
-}
-
-//Rest
-dependencies {
     //okhttp
     //implementation 'com.squareup.okhttp3:okhttp:4.9.2'
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
@@ -102,30 +96,21 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-}
-
-//Images Compose
-dependencies {
+    //Images Compose
     implementation("io.coil-kt:coil-compose:2.4.0")
-}
 
-//Room Db
-dependencies {
+    //Room Db
     // Reference: https://dagger.dev/hilt/gradle-setup
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
 
-// optional - Kotlin Extensions and Coroutines support for Room
+    // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:2.6.1")
-}
 
-//Kotlin Components and Kotlin Coroutines
-dependencies {
+    //Kotlin Components and Kotlin Coroutines
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-}
 
-//ConstraintLayout
-dependencies {
+    //ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 }
